d2144f599037f2f377c07e98852e876ab4da27b5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-04 16:21:34 +0100

    add Name component

---
+++ src/Components/Preview/Name.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Name = ({
+  firstName,
+  lastName,
+  middleName,
+}: {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+}) => {
+  return <div></div>;
+};
+
+export default Name;
--- src/Components/Preview/Preview.tsx
+++ src/Components/Preview/Preview.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import CvData from "../../Types/CvData";
+import Name from "./Name";
 
 const Preview = ({ data }: { data: CvData }) => {
-  return <div></div>;
+  return (
+    <div>
+      <Name
+        firstName={data.name.first}
+        lastName={data.name.last}
+        middleName={data.name.middle}
+      />
+    </div>
+  );
 };
 
 export default Preview;
