6ff9a4f47aa142c336280ea1bcd247e0855b9c92
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-04 16:05:54 +0100

    add cvdata proptype

---
+++ src/Types/CvData.ts
@@ -0,0 +1,47 @@
+type CvData = {
+  name: {
+    first: string;
+    last: string;
+    middle: string;
+  };
+  phoneNumber: string;
+  email: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  profession: string;
+  website: string;
+  about: string;
+  socialLinks: {
+    facebook: string;
+    twitter: string;
+    linkedin: string;
+    instagram: string;
+    github: string;
+    youtube: string;
+  };
+  education: {
+    school: string;
+    degree: string;
+    fieldOfStudy: string;
+    grade: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+  }[];
+  experience: {
+    company: string;
+    position: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+  }[];
+  skills: {
+    skill: string;
+    level: string;
+    yearsOfExperience: string;
+  }[];
+};
+
+export default CvData;
