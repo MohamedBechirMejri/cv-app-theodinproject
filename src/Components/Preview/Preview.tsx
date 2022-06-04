bb5430a40b95c15c543062fe6b74ca8df1647581
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-04 18:56:18 +0100

    do some positioning

--- src/Components/Preview/Preview.tsx
+++ src/Components/Preview/Preview.tsx
@@ -9,19 +9,24 @@ import Experience from "./Experience";
 
 const Preview = ({ data }: { data: CvData }) => {
   return (
-    <div className="select-none">
-      <Name
-        firstName={data.name.first}
-        lastName={data.name.last}
-        middleName={data.name.middle}
-      />
-      <Address
-        address={data.address}
-        city={data.city}
-        state={data.state}
-        country={data.country}
-      />
-      <ContactInfo phoneNumber={data.phoneNumber} email={data.email} />
+    <div className="flex flex-col items-center max-w-6xl gap-16 p-8 px-16 m-auto select-none">
+      <div className="flex items-end gap-8">
+        {" "}
+        <Name
+          firstName={data.name.first}
+          lastName={data.name.last}
+          middleName={data.name.middle}
+        />
+        <div className="flex items-end justify-between w-full gap-10 ">
+          <ContactInfo phoneNumber={data.phoneNumber} email={data.email} />{" "}
+          <Address
+            address={data.address}
+            city={data.city}
+            state={data.state}
+            country={data.country}
+          />
+        </div>
+      </div>
       <About about={data.about} />
       <Education education={data.education} />
       <Experience experience={data.experience} />
