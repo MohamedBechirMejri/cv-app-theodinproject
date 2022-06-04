6de4eb47503645cdf3341b30f5c0865100b0a76f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-04 15:52:26 +0100

    add data placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,45 @@
 import React from "react";
 
 function App() {
+  const [data, setData] = React.useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    address: "",
+    city: "",
+    state: "",
+    country: "",
+    profession: "",
+    website: "",
+    about: "",
+    education: [
+      {
+        school: "",
+        degree: "",
+        fieldOfStudy: "",
+        grade: "",
+        description: "",
+        startDate: "",
+        endDate: "",
+      },
+    ],
+    experience: [
+      {
+        company: "",
+        position: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+      },
+    ],
+    skills: [
+      {
+        skill: "",
+        level: "",
+        yearsOfExperience: "",
+      },
+    ],
+  });
   return <div className="App"></div>;
 }
 
