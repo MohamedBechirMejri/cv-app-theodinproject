eafa08e2970811a389c0bf640813d85f4a21f1b2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-06 21:26:12 +0100

    increase size

--- src/Components/Edit/Button.tsx
+++ src/Components/Edit/Button.tsx
@@ -13,7 +13,7 @@ const Button = ({
     <button
       className={
         ClassName +
-        " bg-yellow-400 px-3 font-light transition-all relative overflow-hidden before:absolute before:transition-transform before:-z-10 before:inset-0 before:bg-black hover:text-white before:duration-[350ms] before:ease-in-out duration-[350ms] hover:before:scale-x-100 hover:before:origin-left before:origin-right before:scale-x-0 [transform-style:preserve-3d] -skew-x-12 active:scale-95"
+        " bg-yellow-400 px-3 font-light transition-all relative overflow-hidden before:absolute before:transition-transform before:-z-10 before:inset-0 before:bg-black hover:text-white before:duration-[350ms] before:ease-in-out duration-[350ms] hover:before:scale-x-100 hover:before:origin-left before:origin-right before:scale-x-0 [transform-style:preserve-3d] -skew-x-12 active:scale-95 text-xl"
       }
       onClick={e => handler(e)}
     >
