d1d9d118a1cbe9288dd235c3555f018f1b91a675
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-06 13:11:26 +0100

    add profession and wesite

--- src/Components/Preview/Name.tsx
+++ src/Components/Preview/Name.tsx
@@ -4,16 +4,30 @@ const Name = ({
   firstName,
   lastName,
   middleName,
+  profession,
+  website,
 }: {
   firstName: string;
   lastName: string;
   middleName: string;
+  profession: string;
+  website: string;
 }) => {
   return (
-    <h1 className="flex flex-col items-end w-max font-bold text-4xl">
+    <h1 className="flex flex-col items-end text-4xl font-bold w-max">
       <span className="bg-yellow-400">{firstName} </span>
       {middleName && <span> {middleName}</span>}
       <span> {lastName}</span>
+      <span className="relative py-2 text-lg font-normal tracking-widest ">
+        {profession}
+        <span className="absolute ml-14 font-bold [letter-spacing:.2rem] lowercase">
+          www.
+          {website
+            .replace("https://", "")
+            .replace("www", "")
+            .replace("http://", "")}
+        </span>
+      </span>
     </h1>
   );
 };
