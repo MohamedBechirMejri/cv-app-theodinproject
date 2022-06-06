d25fb9484e374aae5df73385406dbb698f5747c8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-06 13:10:58 +0100

    tweak styling

--- src/Components/Preview/About.tsx
+++ src/Components/Preview/About.tsx
@@ -3,7 +3,7 @@ import { IoMdQuote } from "react-icons/io";
 
 const About = ({ about }: { about: string }) => {
   return (
-    <div className="flex items-stretch gap-16 font-medium text-black">
+    <div className="flex items-stretch pl-6 font-medium text-black gap-14">
       <div className="flex items-stretch gap-4 text-3xl">
         <IoMdQuote />
         <div className="p-[2.5px] bg-black rounded-full" />
