0232fa58f404455abf4e4b5e6c7024ac9f0b42fc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-06 19:46:01 +0100

    style buttons

--- src/Components/Edit/Button.tsx
+++ src/Components/Edit/Button.tsx
@@ -10,7 +10,13 @@ const Button = ({
   text?: string;
 }) => {
   return (
-    <button className={ClassName + ""} onClick={e => handler(e)}>
+    <button
+      className={
+        ClassName +
+        " bg-yellow-400 px-3 font-light transition-all relative overflow-hidden before:absolute before:transition-transform before:-z-10 before:inset-0 before:bg-black hover:text-white before:duration-[350ms] before:ease-in-out duration-[350ms] hover:before:scale-x-100 hover:before:origin-left before:origin-right before:scale-x-0 [transform-style:preserve-3d] -skew-x-12 active:scale-95"
+      }
+      onClick={e => handler(e)}
+    >
       {text}
     </button>
   );
