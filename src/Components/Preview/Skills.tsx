18adc5e2d6d3f2518ac1c05815db624cd9f4ec73
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-06 12:06:39 +0100

    remove langs and tools

--- src/Components/Preview/Skills.tsx
+++ src/Components/Preview/Skills.tsx
@@ -2,24 +2,36 @@ import React from "react";
 
 const Skills = ({
   skills,
-  languages,
-  tools,
 }: {
   skills: {
     skill: string;
     level: string;
     yearsOfExperience: string;
   }[];
-  languages: {
-    language: string;
-    proficiency: string;
-  }[];
-  tools: {
-    tool: string;
-    level: number;
-  }[];
 }) => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col max-w-[52rem]  gap-4 text-gray-700">
+      {skills.map((skill, index) => {
+        return (
+          <div key={index} className="flex gap-16 font-medium">
+            <div className="flex flex-col items-end justify-start gap-8">
+              <h1
+                className={`font-bold text-3xl ${
+                  index !== 0 ? "opacity-0" : ""
+                } text-black  tracking-widest uppercase`}
+              >
+                Experience
+              </h1>
+              <p className="flex gap-1"></p>
+            </div>
+            <div className="flex flex-col items-start justify-start gap-4">
+              <div className="font-thin tracking-widest uppercase"></div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Skills;
