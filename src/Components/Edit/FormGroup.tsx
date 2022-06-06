cff6551b8b6bc24df71014dc8a8bc630e143dc23
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-06 18:48:14 +0100

    add FormGroup

---
+++ src/Components/Edit/FormGroup.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const FormGroup = ({
+  label,
+  f,
+  value,
+  id,
+  ClassName,
+  type,
+  inputClassName,
+}: {
+  label: string;
+  f: any;
+  value: any;
+  id: string;
+  ClassName: string;
+  type: string;
+  inputClassName: string;
+}) => {
+  return (
+    <div className={"" + ClassName}>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        className={"" + inputClassName}
+        id={id}
+        placeholder={label}
+        value={value}
+        onChange={e => f(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default FormGroup;
